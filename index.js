0aed2cece871b560d694b3f55b6baeadc170238e
AReshetnyak <areshetnyak@qualium-systems.com.ua>
2018-09-13 15:58:01 +0300

    was installed webworker

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 // ! To run Apache Benchmark run in terminal -   ab -c 50 -n 500 localhost:3000/fast
+// * pm2 start index.js -i 0      pm2 show index   pm2 monit  pm2 ls 
 
-const cluster = require('cluster');
 const express = require('express');
 const app = express();
 
 // * If the file being executed in master mode?
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but
-  // in child mode
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-} else {
-  // I'm child, I'm going to act like a server and do nothing else
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {};
-  }
 
-  app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
-  });
-
-  app.get('/fast', (req, res) => {
-    res.send('Hi there FAST');
-  });
-
-  app.listen(3000);
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {};
 }
+
+app.get('/', (req, res) => {
+  doWork(5000);
+  res.send('Hi there');
+});
+
+app.get('/fast', (req, res) => {
+  res.send('Hi there FAST');
+});
+
+app.listen(3001);
