38c627b326b388f5ebcde92425973f6524076cd2
AReshetnyak <areshetnyak@qualium-systems.com.ua>
2018-09-21 18:28:35 +0300

    added upload route

---
+++ NODE STARTER/routes/uploadRoutes.js
@@ -0,0 +1,15 @@
+  
+const AWS = require('aws-sdk');
+const keys = require('../config/keys');
+
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey
+});
+
+module.exports = app => {
+  app.get('api/upload', (req, res) => {
+    const bucketName = 'node-advanced';
+    s3.getSignedUrl('putObject')
+  });
+}
