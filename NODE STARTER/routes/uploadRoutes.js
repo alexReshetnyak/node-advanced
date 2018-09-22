3978fa7d575a6cc6ce964502ed56ad2ca8b52ffd
A Reshetnyak <alexreshetnyakmail@gmail.com>
2018-09-22 16:58:54 +0300

    worked on upload route

--- NODE STARTER/routes/uploadRoutes.js
+++ NODE STARTER/routes/uploadRoutes.js
@@ -1,15 +1,23 @@
   
 const AWS = require('aws-sdk');
+const uuid = require('uuid/v1'); // * Generator uniq user id
+const requireLogin = require('../middlewares/requireLogin');
 const keys = require('../config/keys');
 
 const s3 = new AWS.S3({
   accessKeyId: keys.accessKeyId,
   secretAccessKey: keys.secretAccessKey
 });
+const bucketName = 'node-advanced';
 
 module.exports = app => {
-  app.get('api/upload', (req, res) => {
-    const bucketName = 'node-advanced';
-    s3.getSignedUrl('putObject')
+  app.get('/api/upload', requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+
+    s3.getSignedUrl('putObject', {
+      Bucket: bucketName,
+      ContentType: 'jpeg',
+      Key: key
+    }, (err, url) => res.send({ key, url }))
   });
 }
