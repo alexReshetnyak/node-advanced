9f390cc3891ac1096b502058a355b1f0a88086a4
A Reshetnyak <alexreshetnyakmail@gmail.com>
2018-09-23 14:36:21 +0300

    added image upload logic

--- NODE STARTER/client/src/components/blogs/BlogShow.js
+++ NODE STARTER/client/src/components/blogs/BlogShow.js
@@ -3,8 +3,24 @@ import { connect } from 'react-redux';
 import { fetchBlog } from '../../actions';
 
 class BlogShow extends Component {
+  state = { 
+    imagesUrlPath: 'https://s3.eu-west-3.amazonaws.com/node-advanced/'
+  };
+
+
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
+  }
+
+  renderImage() {
+    console.log(this.state);
+    if (this.props.blog.imageUrl) {
+      return (
+        <div>
+          <img src={ this.state.imagesUrlPath + this.props.blog.imageUrl }></img>
+        </div>
+      )
+    }
   }
 
   render() {
@@ -18,6 +34,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        { this.renderImage() }
       </div>
     );
   }
--- NODE STARTER/routes/uploadRoutes.js
+++ NODE STARTER/routes/uploadRoutes.js
@@ -5,14 +5,52 @@ const requireLogin = require('../middlewares/requireLogin');
 const keys = require('../config/keys');
 
 const s3 = new AWS.S3({
+  endpoint: 's3-eu-west-3.amazonaws.com',
   accessKeyId: keys.accessKeyId,
-  secretAccessKey: keys.secretAccessKey
+  secretAccessKey: keys.secretAccessKey,
+  signatureVersion: 'v4',
+  region: 'eu-west-3'
 });
+
+const amazonPolicySettings = `{
+  "Version": "2012-10-17",
+  "Id": "Policy1537698898229",
+  "Statement": [
+      {
+          "Sid": "Stmt1537698896278",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": "s3:GetObject",
+          "Resource": "arn:aws:s3:::node-advanced/*"
+      }
+  ]
+}`;
+
+const amazonCorsSettings = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+    <CORSRule>
+      <AllowedOrigin>http://localhost:3000</AllowedOrigin>
+      <AllowedMethod>PUT</AllowedMethod>
+      <AllowedHeader>*</AllowedHeader>
+    </CORSRule>
+    <CORSRule>
+      <AllowedOrigin>*</AllowedOrigin>
+      <AllowedMethod>GET</AllowedMethod>
+      <AllowedMethod>PUT</AllowedMethod>
+      <AllowedMethod>HEAD</AllowedMethod>
+      <MaxAgeSeconds>3000</MaxAgeSeconds>
+      <ExposeHeader>x-amz-server-side-encryption</ExposeHeader>
+      <AllowedHeader>Authorization</AllowedHeader>
+    </CORSRule>
+  </CORSConfiguration>`;
+
 const bucketName = 'node-advanced';
 
 module.exports = app => {
   app.get('/api/upload', requireLogin, (req, res) => {
     const key = `${req.user.id}/${uuid()}.jpeg`;
+    // const key = 'sample.jpeg'
 
     s3.getSignedUrl('putObject', {
       Bucket: bucketName,
